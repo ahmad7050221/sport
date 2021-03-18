57681776cc5e725f0e3df4abb8ddb2db3d3d938e
ahmad7050221 <ahmadabudames1999@gmail.com>
2021-03-18 16:25:08 +0200

    adding javascript file

---
+++ javascript/app.js
@@ -0,0 +1,21 @@
+
+var favouriteplayer=prompt("who's your favourite player? ");
+if (favouriteplayer=='messi'){document.write('<img src="https://www.emaratalyoum.com/polopoly_fs/1.1442213.1610441243!/image/image.jpg"/>')}
+else if (favouriteplayer=='ronaldo'){document.write('<img src="https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ec593cc431fb70007482137%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D1321%26cropX2%3D3300%26cropY1%3D114%26cropY2%3D2093"/>')}
+else{alert('choose between messi or ronaldo next time')}
+
+
+var favouriteclub=prompt("who's your favouriteclub?");
+
+if(favouriteclub=="united"){
+    document.write('<h1>welcome united club</h1>');
+}
+else {
+    alert('only united');
+}
+var liking=confirm('do you like messi ?');
+if(liking==true)
+{document.write('better see him in barca');
+}else if(liking==false)
+{
+    alert('dont liking be in another club');}
